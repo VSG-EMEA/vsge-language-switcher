617f775d738e55829dd704f4d7bcba2a4b564273
Erik Golinelli <erik@codekraft.it>
2022-08-29 21:57:44 +0200

    Enhanced backend render (with a dual mode dropdown/modal) adds a static svg icon inside the widget

--- src/edit.js
+++ src/edit.js
@@ -2,39 +2,59 @@ import { InspectorControls, useBlockProps } from '@wordpress/block-editor';
 import { __ } from '@wordpress/i18n';
 import {Panel, PanelBody, PanelRow, SelectControl, TextControl} from '@wordpress/components';
 
-export const Edit = ( { style, attributes: {displayAs}, setAttributes } ) => {
-	return (
+export const Edit = ( props ) => {
+  const {
+    style,
+    attributes: {
+      displayAs,
+      buttonIcon
+    },
+    setAttributes
+  } = props;
+
+  return (
     <div
       { ...useBlockProps( {
         style: { style },
       } ) }
     >
-			<InspectorControls key="setting">
-				<Panel header="Settings">
-					<PanelBody
-						title="Block Settings"
-						icon={ 'settings' }
-						initialOpen={ true }
-					>
-						<PanelRow>
+      <InspectorControls key="setting">
+        <Panel header="Settings">
+          <PanelBody
+            title="Block Settings"
+            icon={ 'settings' }
+            initialOpen={ true }
+          >
+            <PanelRow>
               <SelectControl
                 value={ displayAs }
                 label={ __( 'Display as:' ) }
-                onChange={ ( e ) =>
+                onChange={ ( value ) =>
                   setAttributes( {
-                    displayAs: e
+                    displayAs: value
                   } )
-                 }
+                }
                 options={ [
                   { value: 'modal', label: 'Modal Window' },
                   { value: 'dropdown', label: 'Dropdown' },
                 ] }
               ></SelectControl>
-						</PanelRow>
-					</PanelBody>
-				</Panel>
-			</InspectorControls>
-			<p>Language Switcher</p>
-		</div>
-	);
+            </PanelRow>
+            <PanelRow>
+              <TextControl
+                value={ buttonIcon }
+                label={ __( 'Icon svg (UNESCAPED!)' ) }
+                onChange={ ( value ) =>
+                  setAttributes( {
+                    buttonIcon: value
+                  } )
+                }
+              />
+            </PanelRow>
+          </PanelBody>
+        </Panel>
+      </InspectorControls>
+      <div><i dangerouslySetInnerHTML={{__html: buttonIcon}} ></i>Language Switcher</div>
+    </div>
+  );
 };
--- src/save.js
+++ src/save.js
@@ -1,9 +1,19 @@
 import { useBlockProps } from '@wordpress/block-editor';
 
-export const Save = ( { attributes } ) => {
-	return (
-		<div { ...useBlockProps.save() }>
-      Language Switcher
-		</div>
-	);
+export const Save = ( {
+  style,
+  attributes: {
+    buttonIcon
+  }
+  } ) => {
+  return (
+    <div { ...useBlockProps.save( {
+      style: { style },
+    } ) }
+      id={"pls-language-switcher"}
+    >
+      <i dangerouslySetInnerHTML={{__html: buttonIcon}} ></i>
+      [pls_switcher]
+    </div>
+  );
 };
