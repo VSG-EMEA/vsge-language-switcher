02b1d2b4eacdb0c3eb791765c027fff9960551e6
Erik Golinelli <erik@codekraft.it>
2022-09-08 19:40:42 +0200

    Icon line height fix

--- build/polylang-language-switcher-editor.asset.php
+++ build/polylang-language-switcher-editor.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '7b5ef7e2d8b713820772');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '2353843ad745d1888046');
