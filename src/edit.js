0ad6de47f2980deeab850737e0c4daedac1ac70e
Erik Golinelli <erik@codekraft.it>
2023-03-07 16:33:37 +0100

    v0.0.2 == CHANGELOG == - updated coding standards - renamed to "polylang/language-switcher"

--- src/edit.js
+++ src/edit.js
@@ -1,59 +1,63 @@
-import { InspectorControls, useBlockProps} from '@wordpress/block-editor';
+import { InspectorControls, useBlockProps } from '@wordpress/block-editor';
 import { __ } from '@wordpress/i18n';
-import { Panel, PanelBody, PanelRow, SelectControl, TextControl} from '@wordpress/components';
+import {
+	Panel,
+	PanelBody,
+	PanelRow,
+	SelectControl,
+	TextControl,
+} from '@wordpress/components';
 
 export const Edit = ( props ) => {
-  const {
-    style,
-    attributes: {
-      displayAs,
-      buttonIcon
-    },
-    canUserEdit,
-    setAttributes
-  } = props;
+	const {
+		style,
+		attributes: { displayAs, buttonIcon },
+		canUserEdit,
+		setAttributes,
+	} = props;
 
-  return (
-    <div
-      { ...useBlockProps() }
-    >
-      <InspectorControls key="setting">
-        <Panel header="Settings">
-          <PanelBody
-            title="Block Settings"
-            icon={ 'settings' }
-            initialOpen={ true }
-          >
-            <PanelRow>
-              <SelectControl
-                value={ displayAs }
-                label={ __( 'Display as:' ) }
-                onChange={ ( value ) =>
-                  setAttributes( {
-                    displayAs: value
-                  } )
-                }
-                options={ [
-                  { value: 'modal', label: 'Modal Window' },
-                  { value: 'dropdown', label: 'Dropdown' },
-                ] }
-              ></SelectControl>
-            </PanelRow>
-            <PanelRow>
-              <TextControl
-                value={ buttonIcon }
-                label={ __( 'Icon svg (UNESCAPED!)' ) }
-                onChange={ ( value ) =>
-                  setAttributes( {
-                    buttonIcon: value
-                  } )
-                }
-              />
-            </PanelRow>
-          </PanelBody>
-        </Panel>
-      </InspectorControls>
-      <div><i dangerouslySetInnerHTML={{__html: buttonIcon}} ></i>Language Switcher</div>
-    </div>
-  );
+	return (
+		<div { ...useBlockProps() }>
+			<InspectorControls key="setting">
+				<Panel header="Settings">
+					<PanelBody
+						title="Block Settings"
+						icon={ 'settings' }
+						initialOpen={ true }
+					>
+						<PanelRow>
+							<SelectControl
+								value={ displayAs }
+								label={ __( 'Display as:' ) }
+								onChange={ ( value ) =>
+									setAttributes( {
+										displayAs: value,
+									} )
+								}
+								options={ [
+									{ value: 'modal', label: 'Modal Window' },
+									{ value: 'dropdown', label: 'Dropdown' },
+								] }
+							></SelectControl>
+						</PanelRow>
+						<PanelRow>
+							<TextControl
+								value={ buttonIcon }
+								label={ __( 'Icon svg (UNESCAPED!)' ) }
+								onChange={ ( value ) =>
+									setAttributes( {
+										buttonIcon: value,
+									} )
+								}
+							/>
+						</PanelRow>
+					</PanelBody>
+				</Panel>
+			</InspectorControls>
+			<div>
+				<i dangerouslySetInnerHTML={ { __html: buttonIcon } }></i>
+				Language Switcher
+			</div>
+		</div>
+	);
 };
--- src/save.js
+++ src/save.js
@@ -1,18 +1,13 @@
 import { useBlockProps } from '@wordpress/block-editor';
 
-export const Save = ( {
-  style,
-  attributes: {
-    buttonIcon
-  }
-  } ) => (
-    <button { ...useBlockProps.save( {
-      style: { style },
-    } ) }
-      id={"pls-language-switcher"}
-    >
-      <i dangerouslySetInnerHTML={{__html: buttonIcon}}></i>
-      [pls_switcher]
-    </button>
-  );
-
+export const Save = ( { style, attributes: { buttonIcon } } ) => (
+	<button
+		{ ...useBlockProps.save( {
+			style: { style },
+		} ) }
+		id={ 'pls-language-switcher' }
+	>
+		<i dangerouslySetInnerHTML={ { __html: buttonIcon } }></i>
+		[pls_switcher]
+	</button>
+);
