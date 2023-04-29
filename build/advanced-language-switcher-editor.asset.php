267f20b7a4a8f78df7ef92703701ccf58812c37c
Erik Golinelli <erik@codekraft.it>
2023-04-29 14:59:35 +0200

    fixes frontend style

--- build/advanced-language-switcher-editor.asset.php
+++ build/advanced-language-switcher-editor.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '57062939e96fa5a96706');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => 'acda2bcb4bd6d2f61685');
