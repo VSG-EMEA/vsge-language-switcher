6449a23c81590b5da8ffae0c80f361a9256e7fde
Erik Golinelli <erik@codekraft.it>
2024-03-05 18:12:11 +0100

    vsge-language-switcher fix style enqueue priority

--- inc/enqueue.php
+++ inc/enqueue.php
@@ -2,6 +2,7 @@
 
 function vls_enqueue_scripts_modal() {
 	$asset = include VLS_PLUGIN_DIR . '/build/vsge-language-switcher.asset.php';
+	wp_enqueue_style( 'vsge-language-switcher-style', VLS_PLUGIN_URL . 'build/style-vsge-language-switcher.css' );
 
 	wp_enqueue_script( 'vsge-language-switcher', VLS_PLUGIN_URL . 'build/vsge-language-switcher.js', $asset['dependencies'], false, true );
 	wp_localize_script( 'vsge-language-switcher', 'languageSwitcher', array(
@@ -13,5 +14,4 @@ function vls_enqueue_scripts_modal() {
 		'cookieDomain' => COOKIE_DOMAIN,
 		'namespace'    => VLS_NAMESPACE
 	) );
-	wp_enqueue_style( 'vsge-language-switcher-style', VLS_PLUGIN_URL . 'build/style-vsge-language-switcher.css' );
 }
